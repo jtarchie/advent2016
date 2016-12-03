9773acd15618c92f1196bdc01e1d7fae4a3ae9ac
JT Archie <jtarchie@JTs-MacBook-Air.local>
2016-12-02 21:05:47 -0700

    part b

--- day1.rb
+++ day1.rb
@@ -3,10 +3,13 @@ directions = [[1,0],[0,1],[-1,0],[0,-1]]
 start_pos  = [[0, 0], 0]
 
 steps = "L3, R2, L5, R1, L1, L2, L2, R1, R5, R1, L1, L2, R2, R4, L4, L3, L3, R5, L1, R3, L5, L2, R4, L5, R4, R2, L2, L1, R1, L3, L3, R2, R1, L4, L1, L1, R4, R5, R1, L2, L1, R188, R4, L3, R54, L4, R4, R74, R2, L4, R185, R1, R3, R5, L2, L3, R1, L1, L3, R3, R2, L3, L4, R1, L3, L5, L2, R2, L1, R2, R1, L4, R5, R4, L5, L5, L4, R5, R4, L5, L3, R4, R1, L5, L4, L3, R5, L5, L2, L4, R4, R4, R2, L1, L3, L2, R5, R4, L5, R1, R2, R5, L2, R4, R5, L2, L3, R3, L4, R3, L2, R1, R4, L5, R1, L5, L3, R4, L2, L2, L5, L5, R5, R2, L5, R1, L3, L2, L2, R3, L3, L4, R2, R3, L1, R2, L5, L3, R4, L4, R4, R3, L3, R1, L3, R5, L5, R1, R5, R3, L1".split(", ")
-steps.each do |step|
+coords = steps.map do |step|
   dir, length = *step.match(/(\w)(\d+)/).captures
   start_pos[1] += dir == "R" ? 1 : -1
-  start_pos[0] = (Matrix[start_pos[0]] + Matrix[directions[start_pos[1] % 4]] * length.to_i).to_a.first
-end
+  length.to_i.times.map do
+    start_pos[0] = (Matrix[start_pos[0]] + Matrix[directions[start_pos[1] % 4]]).to_a.first
+  end
+end.flatten(1)
 
 puts start_pos[0].map(&:abs).reduce(:+)
+puts coords.select{ |e| coords.count(e) > 1 }.first.map(&:abs).reduce(:+)
